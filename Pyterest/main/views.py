cae0899b4de58457412963b5297b849bfd5fbe2f
Stanley Destrée <stanleydestree@gmail.com>
2021-04-25 13:01:16 +0200

    shithappened

--- Pyterest/main/views.py
+++ Pyterest/main/views.py
@@ -9,27 +9,27 @@ from .forms import CreateNewList
 def index(response,id):
     ls = ToDoList.objects.get(id=id)
 
-    if response.method=="POST":
-            print(response.POST)
-            if response.POST.get("save"):
-                for item in ls.item_set.all():
-                    if response.POST.get("c"+str(item.id))=="clicked":
-                        print(item)
-                        item.complete = True
+    if ls in response.user.toDoist.all():
+        if response.method=="POST":
+                print(response.POST)
+                if response.POST.get("save"):
+                    for item in ls.item_set.all():
+                        if response.POST.get("c"+str(item.id))=="clicked":
+                            print(item)
+                            item.complete = True
+                        else:
+                            print(item)
+                            item.complete = False
+                    item.save()
+                elif response.POST.get("newItem"):
+                    txt = response.POST.get("new")
+                    if len(txt)>2:
+                        ls.item_set.create(text=txt,complete=False)
                     else:
-                        print(item)
-                        item.complete = False
-                item.save()
-            elif response.POST.get("newItem"):
-                txt = response.POST.get("new")
-                if len(txt)>2:
-                    ls.item_set.create(text=txt,complete=False)
-                else:
-                    print("invalid")
+                        print("invalid")
 
-
-    return render(response, "main/list.html",{"ls":ls})
-    
+        return render(response, "main/list.html",{"ls":ls})
+    return render(response, "main/view.html",{})   
 def home(response):
     return render(response, "main/home.html",{})
 
@@ -39,8 +39,10 @@ def create(response):
 
         if form.is_valid():
             n = form.cleaned_data['name']
+            t=ToDoList(name=n)
+            t.save()
             #save to a specific user
-            response.user.toDoList_set.create(name=n)
+            response.user.ToDoList.add(t)
 
         return HttpResponse('/%i' %t.id)
     else:
